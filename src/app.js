4c98a87d33d59b0c85586f852c03638e774cdb0f
Benny Daon <bennydaon@gmail.com>
2020-04-21 07:59:43 +0300

    Using webexec http server

--- src/app.js
+++ src/app.js
@@ -16,63 +16,61 @@ import env from "env"
 import { Terminal } from 'xterm'
 
 const term = new Terminal()
+let state = 0
+let lastWord = ""
+
 
 const pane0 = document.getElementById('pane0')
-term.open(document.getElementById('pane0'))
-term.write("Starting Connection\r\n")
-
-term.focus()
-
-/*
-let peer = new Peer('ttmux', {host: 'localhost', port: 9000, path: '/'})
-
-peer.on('connection', function (conn) {
-    conn.on('data', function (data) {
-        term.write(data)
-    })
-})
-*/
-let pc = new RTCPeerConnection({
-  iceServers: [
-    {
-      urls: 'stun:stun.l.google.com:19302'
+term.on('key', (keys, ev) => {
+    let key = keys.charCodeAt(0)
+    term.write(key)
+    if (key == 13) {
+        let pc = new RTCPeerConnection({
+            iceServers: [
+              {
+                urls: 'stun:stun.l.google.com:19302'
+              }
+            ]
+        })
+        let sendChannel = pc.createDataChannel('tmux -c')
+        sendChannel.onclose = () => console.log('sendChannel has closed')
+        sendChannel.onopen = () => console.log('sendChannel has opened')
+        sendChannel.onmessage = e => log(`Message from DataChannel '${sendChannel.label}' payload '${e.data}'`)
+        pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
+        pc.onicecandidate = event => {
+        if (event.candidate === null) {
+          let offer = btoa(JSON.stringify(pc.localDescription))
+          term.write("offer: "+offer)
+          fetch('https://localhost:8888/connect', {
+            headers: { "Content-Type": "application/json; charset=utf-8" },
+            method: 'POST',
+            body: JSON.stringify({Offer: offer}) 
+          }).then(response => response.json())
+            .then(data => {
+              let sd = new RTCSessionDescription(JSON.parse(atob(data)))
+              term.write("sd: " + sd)
+              try {
+                pc.setRemoteDescription(sd)
+              } catch (e) {
+                alert(e)
+        }})}}
+        pc.onnegotiationneeded = e =>
+        pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log)
     }
-  ]
+    else
+        lastWord += e.key
 })
-let log = msg => {
-  term.write(msg + '\r\n')
-}
-
-let sendChannel = pc.createDataChannel('tmux -v')
-sendChannel.onclose = () => console.log('sendChannel has closed')
-sendChannel.onopen = () => {
-    console.log('sendChannel has opened')
-    sendChannel.send("password")
-}
-sendChannel.onmessage = e => term.write(e.data)
-term.onData(d =>  sendChannel.send(d))
-
-pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
-pc.onicecandidate = event => {
-  if (event.candidate === null) {
-      term.write(btoa(JSON.stringify(pc.localDescription)))
-  }
-}
-
-pc.onnegotiationneeded = e =>
-  pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log)
+term.open(document.getElementById('pane0'))
+term.write("Welcome To Terminal 7!\n")
+term.write("\nWhere is your host: ")
 
 window.sendMessage = () => {
   let message = document.getElementById('message').value
   if (message === '') {
     return alert('Message must not be empty')
   }
+  sendChannel.send(message)
 }
 
-window.startSession = (sd) => {
-  try {
-    pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(sd))))
-  } catch (e) {
-    alert(e)
-  }
-}
+
+term.focus()
