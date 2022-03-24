09bc45dd7ee5f8aae923edc2cc7e13f98e6ffeba
Benny Daon <benny@tuzig.com>
2022-03-24 13:40:05 +0200

    Improve the mock

--- src/__mocks__/sshsession.ts
+++ src/__mocks__/sshsession.ts
@@ -1,30 +1,38 @@
-import { Session, Channel } from "../src/session.ts"
+import { Session, Channel, State, CallbackType } from "../session.ts"
+
+const returnLater = (ret: unknown) => 
+    vi.fn(() => new Promise( resolve => setTimeout(() => resolve(ret), 0)))
 
 class MockChannel implements Channel {
     id = 1
     onClose: CallbackType
-    onData: CallbackType
-    close = vi.fn(_ => new Promise(resolve => setTimeout(_ => resolve(), 0)))
-    send = vi.fn(_ => new Promise(resolve => setTimeout(_ => resolve(), 0)))
-    resize = vi.fn(_ => new Promise(resolve => setTimeout(_ => resolve(), 0)))
+    onMessage: CallbackType
+    close = returnLater(undefined)
+    send = vi.fn()
+    resize = returnLater(undefined)
+    get readyState(): string {
+        return "open"
+    }
 }
 
 export class SSHSession implements Session {
     onStateChange: (state: State) => void
     onPayloadUpdate: (payload: string) => void
-    constructor(address: string, username: string, password: string, port?: number=22) {
+    constructor(address: string, username: string, password: string, port?=22) {
+        console.log("New seesion", address, username, password, port)
     }
-    connect = vi.fn(() => setTimeout(_ => this.onStateChange("connected"), 0))
-    openChannel = vi.fn((cmd: string, parent: ChannelID, sx?: number, sy?: number) => {
-        return new Promise(resolve => {
-            setTimeout(_ => {
-                let c = new MockChannel()
+    connect = vi.fn(() => setTimeout(() => this.onStateChange("connected"), 0))
+    openChannel = vi.fn(
+        (cmd: string, parent: ChannelID, sx?: number, sy?: number) =>
+        new Promise(resolve => {
+            setTimeout(() => {
+                const c = new MockChannel()
                 resolve(c)
             }, 0)
         })
-    })
-    close = vi.fn(_ => new Promise(resolve => setTimeout(_ => resolve(), 0)))
-    getPayload = vi.fn(_ => new Promise(resolve => setTimeout(_ => resolve(null), 0)))
-    setPayload = vi.fn(s => new Promise(resolve => setTimeout(_ => resolve(null), 0)))
-    disconnect = vi.fn(_ => new Promise(resolve => setTimeout(_ => resolve(null), 0)))
+    )
+    close = returnLater(undefined)
+    getPayload = returnLater(null)
+    setPayload = returnLater(null)
+    disconnect = returnLater(null)
 }
