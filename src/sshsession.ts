f88694d7186da69215d0e2833a88a898171ab943
Benny Daon <benny@tuzig.com>
2022-03-30 14:29:02 +0300

    Getting SSH session to connect
    but not much more. seeming like we the session key is -1.

--- src/sshsession.ts
+++ src/sshsession.ts
@@ -1,35 +1,50 @@
-import { BaseSession }  from './session.ts' 
+import { SSH, SSHSessionID, TerminalType, SSHSessionByPass } from 'capacitor-ssh-plugin'
+import { Channel, BaseChannel, BaseSession, State }  from './session' 
 
-export interface SSHSessionByPass {
-    address: string
-    username: string
-    password: string
-    port: number
+export class SSHChannel extends BaseChannel {
+    close(): Promise<void> {
+        return SSH.closeShell({channel: this.id})
+    }
+    send(data: string): void {
+        SSH.writeToChannel({channel: this.id, s: data})
+    }
+    resize(sx: number, sy: number): Promise<void> {
+        return SSH.setPtySize({channel: this.id, width: sx, height: sy})
+    }
 }
-
 // SSHSession is an implmentation of a real time session over ssh
 export class SSHSession extends BaseSession {
+    id: SSHSessionID
     byPass: SSHSessionByPass;
-    onStateChange : (state: RTState) => void
+    onStateChange : (state: State) => void
     onPayloadUpdate: (payload: string) => void
-    constructor(address: string, username: string, password: string, port: number=22) {
+    constructor(address: string, username: string, password: string, port=22) {
         super()
-        this.byPass.address = address
-        this.byPass.username = username
-        this.byPass.passwork = password
-        this.byPass.port = port
+        this.byPass = {address: address,
+                       username: username,
+                       password: password,
+                       port: port,
+                      }
     }
     connect() {
-        SSHPlugin.startSessionByPasswd(this.byPass).then(id => {
+        SSH.startSessionByPasswd(this.byPass).then(id => {
             this.id = id
             this.onStateChange("connected")
         })
     }
 
-    openChannel(cmd: string, parent?: RTChannelID, sx?: number, sy?: number):
-        Promise<RTChannel> {
-    }
-    openChannel?(id: RTChannelID): Promise<RTChannel> {
+    openChannel(cmd: string, parent?: ChannelID, sx?: number, sy?: number):
+         Promise<Channel> {
+        return new Promise((resolve, reject) => {
+            const channel = new SSHChannel()
+            SSH.startShell(TerminalType.PtyTerminalXterm, this.id,
+                                 (m) => channel.onMessage(m))
+                     .then(id => {
+                             channel.id = id
+                             SSH.setPtySize({channel: id, width: sx, height: sy})
+                                      .then(() => resolve(channel))
+                     }).catch(reject)
+        })
     }
     close(): Promise<void>{
     }
