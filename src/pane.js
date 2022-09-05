d845c446945d75b1539864440af2b35b0418f5cd
Benny Daon <benny@tuzig.com>
2022-09-05 11:06:48 +0300

    Risking double sessions

--- src/pane.js
+++ src/pane.js
@@ -279,8 +279,6 @@ export class Pane extends Cell {
                 reject("Gate has no session yet")
                 return
             }
-            if (this.d && (this.d.readyState == "open"))
-                return
             this.buffer = []
             if (opts.id) {
                 this.gate.session.openChannel(opts.id)
