4a10897adc211218279009bc157bbad1d798facd
Eyal Sasson <eyals180@gmail.com>
2023-01-12 18:11:12 +0200

    Syncing hourglass with watchdog

--- src/gate.ts
+++ src/gate.ts
@@ -154,7 +154,6 @@ export class Gate {
             return
         }
         this.t7.log(`updating ${this.name} state to ${state} ${failure}`)
-        this.map.shell.stopHourglass()
         if (state == "connected") {
             this.marker = null
             this.notify(`🥂  over ${this.session.isSSH?"SSH":"WebRTC"}`)
@@ -602,7 +601,6 @@ export class Gate {
             this.t7.log("TBD: update layout", layout)
         }
         this.t7.log("opening session")
-        this.map.shell.startHourglass(this.t7.conf.net.timeout)
         if (this.noIds)
             this.session.connect(this.marker)
         else {
