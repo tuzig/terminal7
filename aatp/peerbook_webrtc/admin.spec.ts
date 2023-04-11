2e421b3bd40c3d1fc16a7de63f6c6a456a6fc923
Benny Daon <benny@tuzig.com>
2023-04-11 10:02:02 +0300

    Fixing the admin acceptance test

---
+++ aatp/peerbook_webrtc/admin.spec.ts
@@ -0,0 +1,137 @@
+import { test, expect, Page, BrowserContext } from '@playwright/test'
+import { authenticator } from 'otplib'
+import waitPort from 'wait-port'
+import * as redis from 'redis'
+
+const url = process.env.LOCALDEV?"http://localhost:3000":"http://terminal7"
+
+test.describe('peerbook administration', ()  => {
+
+    const sleep = (ms) => { return new Promise(r => setTimeout(r, ms)) }
+    let redisClient: redis.Redis
+    let page: Page,
+        context: BrowserContext
+
+    async function getTWRBuffer() {
+        return await page.evaluate(() => {
+            const t = window.terminal7.map.t0
+            const b = t.buffer.active
+            let ret = ""
+            for (let i = 0; i < b.length; i++) {
+                const line = b.getLine(i).translateToString()
+                ret += line
+            }
+            return ret
+        })
+    }
+    async function getTWRLastLine() {
+        return await page.evaluate(() => {
+            const t = window.terminal7.map.t0
+            const b = t.buffer.active
+            console.log("t0 buffer len", b.length)
+            return b.getLine(b.length-1).translateToString()
+        })
+    }
+    test.afterAll(async () => await context.close() )
+    test.beforeAll(async ({ browser }) => {
+        context = await browser.newContext()
+        page = await context.newPage()
+        page.on('console', (msg) => console.log('console log:', msg.text()))
+        page.on('pageerror', (err: Error) => console.log('PAGEERROR', err.message))
+        await waitPort({host:'peerbook', port:17777})
+        await waitPort({host:'terminal7', port:80})
+        const response = await page.goto(url)
+        await expect(response.ok(), `got error ${response.status()}`).toBeTruthy()
+        await page.evaluate(async () => {
+            localStorage.setItem("CapacitorStorage.dotfile",`
+[theme]
+foreground = "#00FAFA"
+background = "#000"
+selection = "#D9F505"
+[indicators]
+flash = 100
+[exec]
+shell = "bash"
+[net]
+timeout = 3000
+retries = 3
+ice_server = "stun:stun2.l.google.com:19302"
+peerbook = "peerbook:17777"
+[ui]
+quickest_press = 1000
+max_tabs = 10
+cut_min_distance = 80
+cut_min_speed = 2.5
+pinch_max_y_velocity = 0.1
+[peerbook]
+insecure = true`)
+        })
+        // first page session for just for storing the dotfiles
+        await page.reload({waitUntil: "load"})
+        // add terminal7 initializtion and globblas
+        await waitPort({host:'webexec', port:7777})
+
+        redisClient = redis.createClient({url: 'redis://redis'})
+        redisClient.on('error', err => console.log('Redis client error', err))
+        await redisClient.connect()
+    })
+
+    test('purchase update with no active subscription', async () => {
+        await sleep(100)
+        await page.evaluate(async () => {
+            await window.terminal7.map.shell.onPurchasesUpdate({
+                customerInfo: {
+                    originalAppUserId: "ValidBearer",
+                    entitlements: {active: []},
+                },
+                // purchases: {identifier: "com.terminal7.terminal7.terminal7", purchaseState: 0}
+            })
+        })
+        const twr = await getTWRBuffer()
+        console.log("TWR", twr)
+        await sleep(100)
+        expect(twr).toMatch(/`subscribe`/)
+    })
+    test('purchase update with an active subscription and bad otp', async () => {
+        await sleep(100)
+        await redisClient.set("tempid:ValidBearer", "1")
+        await page.evaluate(async () => {
+            window.terminal7.map.shell.onPurchasesUpdate({
+                customerInfo: {
+                    originalAppUserId: "ValidBearer",
+                    entitlements: {active: { peerbook: {expirationDate: "2021-01-01T00:00:00Z"}}},
+                },
+                // purchases: {identifier: "com.terminal7.terminal7.terminal7", purchaseState: 0}
+            })
+        })
+        await sleep(1500)
+        let twr = await getTWRBuffer()
+        expect(twr).toMatch(/Peer name/)
+        await page.keyboard.type("test")
+        await page.keyboard.press("Enter")
+        await sleep(100)
+        twr = await getTWRBuffer()
+        expect(twr).toMatch(/email/)
+        await page.keyboard.type("foo@bar.com")
+        await page.keyboard.press("Enter")
+        await sleep(1200)
+        twr = await getTWRLastLine()
+        expect(twr).toMatch(/OTP:/)
+        await page.keyboard.type("1234")
+        await page.keyboard.press("Enter")
+        await sleep(100)
+        twr = await getTWRBuffer()
+        expect(twr).toMatch(/Invalid OTP.*OTP:/)
+    })
+    test('complete purchase with a valid OTP', async () => {
+        const uid = await redisClient.get("id:foo@bar.com")
+        const secret = await redisClient.get(`secret:${uid}`)
+        const token = authenticator.generate(secret);
+        await page.keyboard.type(token)
+        await page.keyboard.press("Enter")
+        await sleep(100)
+        let twr = await getTWRBuffer()
+        console.log("TWR", twr)
+        expect(twr).toMatch(/Validated/)
+    })
+})
