c80d84d30bd1865a1003c4a863f3ed2afe964155
Benny Daon <benny@tuzig.com>
2023-08-02 15:13:39 +0300

    Updating tests for a connected unaithorized ws

--- aatp/peerbook_webrtc/admin.spec.ts
+++ aatp/peerbook_webrtc/admin.spec.ts
@@ -75,7 +75,7 @@ insecure = true`)
             }
         })
         const pbOpen = await page.evaluate(() => window.terminal7.pb.isOpen())
-        expect(pbOpen).toBeFalsy()
+        expect(pbOpen).toBeTruthy()
     })
     test('purchase update with an active subscription and bad otp', async () => {
         await sleep(500)
