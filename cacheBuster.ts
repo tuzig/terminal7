62657aab6bb3a1fe4adeff75f033c61ff7c93841
May Shech <may745@cs.colman.ac.il>
2023-12-22 13:25:50 +0200

    Making cache buster change every one hour automatically

---
+++ cacheBuster.ts
@@ -0,0 +1,19 @@
+// cacheBuster.ts
+function generateCacheBuster(): string {
+  // Find the main.ts script tag and remove it
+  const mainScript: HTMLScriptElement | null = document.querySelector('script[src*="main.ts"]');
+  if (mainScript) {
+    mainScript.remove();
+  }
+  // Generate a cache buster value based on the current timestamp
+    const currentTimestamp: number = Math.floor(Date.now() / 1000);
+    const cacheBusterValue: number = Math.floor(currentTimestamp / 3600);
+    return `?v=${cacheBusterValue}`;
+  }
+
+  // Set the src attribute for main.ts script
+  const script: HTMLScriptElement = document.createElement('script');
+  script.type = 'module';
+  script.src = `/main.ts${generateCacheBuster()}`;
+  document.head.appendChild(script);
+  
