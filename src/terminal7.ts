b0dace891c629510759c7a7ae4b108c69adfa386
Benny Daon <benny@tuzig.com>
2024-07-31 18:24:13 +0300

    Polishing

--- src/terminal7.ts
+++ src/terminal7.ts
@@ -429,7 +429,7 @@ export class Terminal7 {
     async pbConnect(): Promise<void> {
         const statusE = document.getElementById("peerbook-status") as HTMLSpanElement
         // TODO: refactor this to an sync function
-        return new Promise((resolve, reject) => {
+        return new Promise<void>((resolve, reject) => {
             const callResolve = () => {
                 statusE.style.opacity = "1"
                 resolve()
@@ -437,15 +437,14 @@ export class Terminal7 {
             const callReject = (e, symbol = ERROR_HTML_SYMBOL) => {
                 statusE.style.opacity = "1"
                 statusE.innerHTML = symbol
+                this.pb.close()
                 terminal7.log("pbConnect failed", e)
                 reject(e)
             }
             const catchConnect = (e: string) => {
                 let symbol = LOCK_HTML_SYMBOL
                 if (e =="Unregistered")
-                    this.notify(Capacitor.isNativePlatform()?
-                        `${PB} You need to register, please \`subscribe\``:
-                        `${PB} You need to regisrer, please \`subscribe\` on your tablet`)
+                    this.notify(`${PB} Unregistered, please \`subscribe\``)
 
                 else if (e == Failure.NotSupported) {
                     // TODO: this should be changed to a notification
@@ -454,11 +453,9 @@ export class Terminal7 {
                     console.log("PB not supported")
                 }
                 else if (e != "Unauthorized") {
-                    terminal7.log("PB connect failed", e)
-                    this.notify(Capacitor.isNativePlatform()?
-                        `${PB} Failed to connect, please try \`subscribe\``:
-                        `${PB} Failed to connect, please try \`login\``)
-                    this.notify("If the problem persists, `support`")
+                    this.log("PB connect failed", e)
+                    this.notify(`${PB} Failed to connect, please try \`subscribe\``)
+                    this.notify("If the problem persists, \`support\`")
                     symbol = ERROR_HTML_SYMBOL
                 }
                 callReject(e, symbol)
@@ -689,7 +686,7 @@ export class Terminal7 {
         this.conf.ui.scrollback = this.conf.ui.scrollback || 10000
 
         this.conf.net = this.conf.net || {}
-        this.conf.net.iceServer = this.conf.net.ice_server || []
+        this.conf.net.iceServer = this.conf.net.ice_server || [ "stun:stun2.l.google.com:19302" ]
         this.conf.net.peerbook = this.conf.net.peerbook ||
             "api.peerbook.io"
         if (this.conf.net.peerbook == "pb.terminal7.dev")
@@ -1175,7 +1172,8 @@ export class Terminal7 {
                 return
             }
             if (!this.pb.session || !this.pb.session.isOpen() ) {
-                resolve([])
+                this.setIceServers([])
+                resolve(this.iceServers)
                 return
             }
             this.pb.session.sendCTRLMsg(new ControlMessage("ice_servers"))
